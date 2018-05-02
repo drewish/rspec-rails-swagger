c9af8443cfef30bf22e39b366b87aa352d37f67c
andrew morton <drewish@katherinehouse.com>
2018-05-02 13:50:52 -0600

    Support YAML output

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -7,11 +7,18 @@ RSpec.configure do |config|
 
   # Define one or more Swagger documents and global metadata for each.
   #
-  # When you run the "swaggerize" rake task, the complete Swagger will be
+  # When you run the "swagger" rake task, the complete Swagger will be
   # generated at the provided relative path under `swagger_root`
+  #
+  # If the file name ends with .yml or .yaml the contents will be YAML,
+  # otherwise the file will be JSON.
+  #
   # By default, the operations defined in spec files are added to the first
-  # document below. You can override this behavior by adding a swagger_doc tag to the
-  # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
+  # document below. You can override this behavior by adding a `swagger_doc` tag
+  # to the the root example_group in your specs, e.g.
+  #
+  #   describe '...', swagger_doc: 'v2/swagger.json'
+  #
   config.swagger_docs = {
     'v1/swagger.json' => {
       swagger: '2.0',
